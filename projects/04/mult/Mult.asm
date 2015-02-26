b551783224d088efb3b8df273ab33549902e219b
Eric Litwin <elitwin@rocketmail.com>
2015-02-25 21:48:04 -0800

    Added Project 04 - Mult

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -6,4 +6,27 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    @R2
+    M=0 // initialize RAM[2] to 0
+
+(LOOP)
+    @R1 // Terminate if R1 <= 0 (can also check if R0 is 0 and terminate early)
+    D=M
+    @END
+    D;JLE
+
+    @R0
+    D=M
+
+    @R2
+    M=M+D // R2+=R0
+
+    @R1
+    M=M-1 // R1-=1 (Decrement counter)
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP // infinite loop to terminate
